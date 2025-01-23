d2bf5a903a77ceb96bda749287b132ef5ebc6d58
Ashish Dhakal <114866470+found-sec@users.noreply.github.com>
2025-01-23 16:33:18 +0300

    Updated App.jsx to integrate feedback page together with other pages.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Homepage from './pages/homepage';
 import Map from './pages/map';
 import Quiz from './pages/quiz';
+import ExperienceForm from './pages/feedback';
 import { TransitionProvider } from './context/TransitionContext';
 
 const App = () => {
@@ -18,17 +19,19 @@ const App = () => {
   const handleBackClick = () => {
     setCurrentPage('home');
   };
+  const handleFeedbackClick = () => {
+    setCurrentPage('feedback');
+  };
 
   return (
     <TransitionProvider>
       <div className="app-container">
         <div className={`pages-wrapper ${currentPage !== 'home' ? 'show-map' : ''}`}>
-          <Homepage 
-            onMapClick={handleMapClick} 
-            onQuizClick={handleQuizClick} 
-          />
+          <Homepage onMapClick={handleMapClick} onQuizClick={handleQuizClick} onFeedbackClick={handleFeedbackClick} />
           {currentPage === 'map' && <Map onBackClick={handleBackClick} />}
           {currentPage === 'quiz' && <Quiz onBackClick={handleBackClick} />}
+          {currentPage === 'feedback' && <ExperienceForm onBackClick={handleBackClick} />}
+
         </div>
       </div>
     </TransitionProvider>
