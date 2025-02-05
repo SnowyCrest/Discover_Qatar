71d324fa6fbf2a34e70a61ef9184c6d359cfbc42
SnowyCrest <anas.timeridjine@gmail.com>
2025-02-05 21:26:34 +0300

    fix(AbstractVectors): resolved bg pattern dir

--- src/components/AbstractVectors.jsx
+++ src/components/AbstractVectors.jsx
@@ -5,7 +5,7 @@ const AbstractVectors = () => {
   return (
     <>
       <div className="pattern-container">
-        <img src="/src/assets/bg_pattern.png" alt="Cannot load pattern" className="bg-pattern" />
+        <img src="/bg_pattern.png" alt="Cannot load pattern" className="bg-pattern" />
       </div>
       <div className="corner-decorations">
         <div className="corner top-left"></div>
